23dd736dca9d0333baec4e8bf153a3a71650e761
ASHISHTIME <155893081+BWFTIME@users.noreply.github.com>
2024-04-08 18:14:47 +0530

    Update start.py

--- AshishX/utils/inline/start.py
+++ AshishX/utils/inline/start.py
@@ -8,9 +8,22 @@ def start_panel(_):
     buttons = [
         [
             InlineKeyboardButton(
-                text=_["S_B_1"], url=f"https://t.me/{app.username}?startgroup=true"
+                text="𝐇𝐞𝐥𝐩 & 𝐂𝐨𝐦𝐦𝐚𝐧𝐝𝐬",
+                url=f"https://t.me/{BOT_USERNAME}?start=help",
+            )
+        ],
+        [
+            InlineKeyboardButton(
+                text="⚙ 𝐁𝐨𝐭 𝐒𝐞𝐭𝐭𝐢𝐧𝐠 ⚙", callback_data="settings_helper"
+            )
+        ],
+        [
+            InlineKeyboardButton(
+                text="💬 𝐒𝐮𝐩𝐩𝐨𝐫𝐭", url=config.SUPPORT_GROUP
             ),
-            InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT),
+            InlineKeyboardButton(
+                text="𝐔𝐩𝐝𝐚𝐭𝐞𝐬 📡", url=config.SUPPORT_CHANNEL
+            )
         ],
     ]
     return buttons
@@ -20,18 +33,34 @@ def private_panel(_):
     buttons = [
         [
             InlineKeyboardButton(
-                text=_["S_B_3"],
-                url=f"https://t.me/{app.username}?startgroup=true",
+                text="🔎 ʜᴇʟᴩ 🔎",
+                callback_data="settings_back_helper",
             )
         ],
-        [InlineKeyboardButton(text=_["S_B_4"], callback_data="settings_back_helper")],
         [
-            InlineKeyboardButton(text=_["S_B_5"], user_id=config.OWNER_ID),
-            InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT),
+            InlineKeyboardButton(
+                text="📨 ᴄʜᴀɴɴᴇʟ", url=config.SUPPORT_CHANNEL
+            ),
+            InlineKeyboardButton(
+                text="📨 sᴜᴘᴘᴏʀᴛ", url=config.SUPPORT_GROUP
+            )
         ],
         [
-            InlineKeyboardButton(text=_["S_B_6"], url=config.SUPPORT_CHANNEL),
-            InlineKeyboardButton(text=_["S_B_7"], url=f"https://t.me/L2R_KING0"),
+            InlineKeyboardButton(
+                text="⛩️ ᴀᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ɢʀᴏᴜᴘ ⛩️",
+                url=f"https://t.me/{BOT_USERNAME}?startgroup=true",
+            )
         ],
-    ]
+        [
+            InlineKeyboardButton(
+                text="🔥 ᴏᴡɴᴇʀ 🔥",
+                user_id=OWNER,
+            )
+        ],
+        [
+            InlineKeyboardButton(
+                text="🇮🇳 ʟᴀɴɢᴜᴀɢᴇ 🏳️‍🌈",
+                callback_data="LG"
+            )
+        ]
     return buttons
